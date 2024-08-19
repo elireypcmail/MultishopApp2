7f97ffb232573df272c120706bbc5d475d9b0ea7
SugeyCh <chaconsugey2003@gmail.com>
2024-08-19 11:46:12 -0400

    changes

--- app/globals/api.js
+++ app/globals/api.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 const instance = axios.create({
   //baseURL: 'http://localhost:4000',
-  baseURL: 'https://multishop-app1-production.up.railway.app/',
+  baseURL: 'https://multishop-app1-production.up.railway.app',
   headers: {
     Accept: "application/json"
   }
