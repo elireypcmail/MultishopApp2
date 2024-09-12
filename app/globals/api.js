b268a4cc72bef31efdb8ae563fb62b41d83a6636
SugeyCh <chaconsugey2003@gmail.com>
2024-09-12 16:00:33 -0400

    The connection to the server on railway has been changed

--- app/globals/api.js
+++ app/globals/api.js
@@ -1,8 +1,8 @@
 import axios from "axios"
 
 const instance = axios.create({
-  baseURL: 'http://localhost:4000',
-  //baseURL: 'https://multishop-app1-production.up.railway.app',
+  //baseURL: 'http://localhost:4000',
+  baseURL: 'https://multishop-app1-production.up.railway.app',
   headers: {
     Accept: "application/json"
   }
