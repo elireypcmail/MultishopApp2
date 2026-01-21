875648d9012cc803bf3211f42460db6bf344d8e1
Diego9714 <diegoacabreram@hotmail.com>
2026-01-20 22:29:05 -0400

    nuevos cambios para unidades en kpis y token v2

--- pages/api/Post.js
+++ pages/api/Post.js
@@ -20,8 +20,6 @@ export const verifyToken = async (token) => {
         Authorization: `Bearer ${token}`
       }
     })
-    console.log(response)
-    
     return response.data
   } catch (error) {
     console.error('Error en la verificación del token:', error)
@@ -39,6 +37,59 @@ export const getParametros = async () => {
   } catch (error) {
     console.log(error)
     console.error('Error en la solicitud de login:', error)
+    throw error
+  }
+}
+
+export const generateToken = async (data) => {
+  try {
+    const response = await instance.post('/generate-code', data)
+    // console.log(response);
+    // console.log(response.status);
+    // console.log(response.data)    
+    return response.data
+  } catch (error) {
+    console.log(error)
+    console.error('Error en la solicitud del code:', error)
+    throw error
+  }
+}
+
+export const validateToken = async (data) => {
+  try {
+    const response = await instance.put('/validate-code', data)
+    // console.log(response);
+    // console.log(response.status);
+    // console.log(response.data)    
+    return response.data
+  } catch (error) {
+    console.log(error)
+    console.error('Error en la solicitud del code:', error)
+    throw error
+  }
+}
+
+// export const verifyTokenCode = async (data) => {
+//   try {
+//     const response = await instance.put('/verify-token', data)
+//     return response.data
+//   } catch (error) {
+//     console.error('Error al consumir el token:', error)
+//     throw error
+//   }
+// }
+
+
+export const disabledToken = async (data) => {
+  try {
+    const response = await instance.put('/disable-code', data)
+    // console.log(response);
+    // console.log(response.status);
+    // console.log(response.data)    
+    return response.data
+  } catch (error) {
+    console.log(error)
+    console.error('Error en la solicitud del code:', error)
     throw error
   }
 }
